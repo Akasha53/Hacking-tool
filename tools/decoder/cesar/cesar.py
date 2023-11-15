31099ebb9b9d0ca71a46df7f8f18ab3279dec8cd
Akasha53 <134799967+Akasha53@users.noreply.github.com>
2023-11-15 14:58:28 +0100

    Update cesar.py

--- tools/decoder/cesar/cesar.py
+++ tools/decoder/cesar/cesar.py
@@ -1,19 +1,10 @@
-message = input("Enter the message you want to encode: ")
-shift = int(input("Enter the shift value (integer): "))
-length = len(message)
+import hashlib
 
-encoded_message = ""
+def md5_encode(message):
+    md5_hash = hashlib.md5(message.encode()).hexdigest()
+    return md5_hash
 
-for x in range(length):
-  char = message[x]
-  if char.isalpha():
-    unicode = ord(char)
-    if char.islower():
-      encoded_unicode = (unicode - 97 + shift) % 26 + 97
-    else:
-      encoded_unicode = (unicode - 65 + shift) % 26 + 65
-    encoded_message += chr(encoded_unicode)
-  else:
-    encoded_message += char
-
-print("Encoded message:", encoded_message)
+# Exemple d'utilisation :
+message_to_encode = input("Enter the message you want to encode: ")
+md5_encoded_message = md5_encode(message_to_encode)
+print("MD5 hash:", md5_encoded_message)
