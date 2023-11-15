5b35813cd92f5630757286a73e253ea9f712b0e8
Akasha53 <134799967+Akasha53@users.noreply.github.com>
2023-11-15 15:01:22 +0100

    Update sha512.py

--- tools/decoder/sha512/sha512.py
+++ tools/decoder/sha512/sha512.py
@@ -1,22 +1,10 @@
 import hashlib
 
-def sha512_encode(message, salt=None):
-    sha512_hash = hashlib.sha512(salt.encode('utf-8')) if salt else hashlib.sha512()
-    sha512_hash.update(message.encode('utf-8'))
-    return sha512_hash.hexdigest()
+def sha512_encode(message):
+    sha512_hash = hashlib.sha512(message.encode()).hexdigest()
+    return sha512_hash
 
-def main():
-    user_input = input("Enter the string to hash with SHA-512: ")
-    use_salt = input("Do you want to use a salt? (yes/no): ").lower()
-
-    if use_salt == 'yes':
-        salt = input("Enter the salt: ")
-        hashed_message = sha512_encode(user_input, salt)
-    else:
-        hashed_message = sha512_encode(user_input)
-
-    print(f"\nOriginal string: {user_input}")
-    print(f"Hashed SHA-512: {hashed_message}")
-
-if __name__ == "__main__":
-    main()
+# Exemple d'utilisation :
+message_to_encode = input("Enter the message you want to encode: ")
+sha512_encoded_message = sha512_encode(message_to_encode)
+print("SHA-512 hash:", sha512_encoded_message)
